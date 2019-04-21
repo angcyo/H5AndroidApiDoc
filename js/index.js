84182ce5adb15047eb822ab1daca1db400a14521
angcyo <angcyo@126.com>
2019-04-21 14:08:22 +0800

    add bootstrap css

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+function doUrl(raw) {
+    return raw.replace(/\s*/g, "").replace(/\./g, "/").replace('/html', '.html')
+}
+
 $(document).ready(() => {
     $('#search').click(() => {
         let input = $('#input').val()
@@ -9,7 +13,22 @@ $(document).ready(() => {
         if (input) {
             // console.log(this)
             // console.log($('#input').val()) android/view/View
-            window.open(`https://developer.android.google.cn/reference/${input.replace(/\s*/g,"").replace(/\./g,"/").replace('/html', '.html')}`)
+            window.open(`https://developer.android.google.cn/reference/${doUrl(input)}`)
+        }
+
+    })
+
+    $('#search_b').click(() => {
+        let input = $('#input_b').val()
+
+        if (!input) {
+            input = 'com.android'
+        }
+
+        if (input) {
+            // console.log(this)
+            // console.log($('#input').val()) android/view/View
+            window.open(`https://jcenter.bintray.com/${doUrl(input)}`)
         }
 
     })
